aaa96b314cdf7d4550cdd94926fa40b585d5fe03
ledkjy <lorenzo.galotti@gmail.com>
2021-06-21 11:05:27 +0200

    syntx fix

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const fetchUsers = async () => await (await fetch('./netlify/functions/getusers')).json();
+const fetchUsers = async () => await (await fetch('/.netlify/functions/getusers')).json();
 
 fetchUsers().then(data => {
   userList = document.querySelector('#users');
